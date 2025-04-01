6477ee32f840dfd0263a03b0ff4dfe524d540b01
Joseph Lee <jhjlee702@gmail.com>
2025-04-01 16:17:07 -0600

    enable BE and FE communication

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,10 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3001'  # this allows the backend to communicate with the front end, bypassing default CORS setting
+    resource '*',
+    headers: :any,
+    expose: ["access-token", "client", "uid"], # expose the tokens
+    methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    credentials: true # allow cookies and auth headers
+  end
+end
