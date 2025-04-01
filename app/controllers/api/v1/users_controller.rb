511d23772c7014df4b9675880ee1312a7d699617
Joseph Lee <jhjlee702@gmail.com>
2025-04-01 17:29:50 -0600

    write CRUD actions for users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,47 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def show
+    render json: @user
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: 422
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: {errors: @user.errors.full_messages }, status: 422
+    end
+  end
+
+  def destroy
+    @user.destroy
+    render json: { message: "User successfully deleted." }, status: 200
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User was not found." }, status: 404
+  end
+
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
