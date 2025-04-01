82cb551ed3d11210feecc8568c90b5a0aeb0149d
Joseph Lee <jhjlee702@gmail.com>
2025-04-01 17:20:45 -0600

    create trips and itineraries resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   mount DeviseTokenAuth::Engine => '/auth'
   
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :trips do
+    resources :itineraries
+  end
 end
