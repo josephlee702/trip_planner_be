5d1bce84393599bb16a95decfac61bf9a0ea5294
Joseph Lee <jhjlee702@gmail.com>
2025-04-01 17:06:23 -0600

    add associations for many to many trips and users

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,4 @@
 class Trip < ApplicationRecord
+  has_many :trips_users
+  has_many :trips, through: :trips_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
+
+  has_many :trips_users
+  has_many :trips, through: :trips_users
 end
