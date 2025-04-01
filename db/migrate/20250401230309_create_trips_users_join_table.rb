85aa0f23746da763e3e65fbebcca34834ee2f349
Joseph Lee <jhjlee702@gmail.com>
2025-04-01 17:04:47 -0600

    create trips users join table migration

--- db/migrate/20250401230309_create_trips_users_join_table.rb
+++ db/migrate/20250401230309_create_trips_users_join_table.rb
@@ -1,4 +1,8 @@
 class CreateTripsUsersJoinTable < ActiveRecord::Migration[7.1]
   def change
+    create_table :trips_users, id: false do |t|
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :trip, foreign_key: true
+    end
   end
 end
