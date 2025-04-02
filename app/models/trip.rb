068a77352923c827a8b53c6ef2ff7dc5e7661270
Joseph Lee <jhjlee702@gmail.com>
2025-04-01 18:12:14 -0600

    add validations and custom validation to trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,15 @@ class Trip < ApplicationRecord
   has_many :trips_users
   has_many :users, through: :trips_users
   has_many :itineraries
+
+  validates :name, presence: true, uniqueness: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate :end_date_after_start_date
+
+  def end_date_after_start_date
+    if end_date.present? && start_date.present? && end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+  end
 end
