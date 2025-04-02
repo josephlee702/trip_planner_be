1a7cc58c044058c4bad988d3a2a80b05a625ec40
Joseph Lee <jhjlee702@gmail.com>
2025-04-01 18:12:01 -0600

    add validations to itinerary and user models

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,3 +1,8 @@
 class Itinerary < ApplicationRecord
   belongs_to :trip
+
+  validates :name, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  extend Devise::Models
   include DeviseTokenAuth::Concerns::User
 
   has_many :trips_users
   has_many :trips, through: :trips_users
+
+  validates :email, presence: true, uniqueness: true
+  #on: :create makes sure the password validation only happens when creating a new user
+  validates :password, presence: true, length: { minimum: 4 }, on: :create
 end
