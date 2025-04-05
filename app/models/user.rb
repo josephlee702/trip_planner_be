eeb0046e2ad31d9be750eab46d027b8be451eec3
Joseph Lee <jhjlee702@gmail.com>
2025-04-05 15:44:50 -0600

    took out manual password validation - devise takes care of this

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  extend Devise::Models
   include DeviseTokenAuth::Concerns::User
 
   has_many :trips_users
@@ -7,5 +6,4 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   #on: :create makes sure the password validation only happens when creating a new user
-  validates :password, presence: true, length: { minimum: 4 }, on: :create
 end
