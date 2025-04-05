85db27b1348cfcbf0e01dd65d5eb8bd1cf501e10
Joseph Lee <jhjlee702@gmail.com>
2025-04-05 16:40:24 -0600

    take out create action since I'm using devise_token_auth's method for user registration

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,15 +10,6 @@ class Api::V1::UsersController < ApplicationController
     render json: @user
   end
 
-  def create
-    user = User.new(user_params)
-    if user.save
-      render json: user, status: :created
-    else
-      render json: { errors: user.errors.full_messages }, status: 422
-    end
-  end
-
   def update
     if @user.update(user_params)
       render json: @user
