bfdfe2eac78c61a8088a6c7104b8e3da19330e6d
Joseph Lee <jhjlee702@gmail.com>
2025-04-05 17:41:46 -0600

    trips users stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_01_233653) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_05_232858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,9 +36,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_01_233653) do
     t.string "destinations", default: [], array: true
   end
 
-  create_table "trips_users", id: false, force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "trip_id"
+  create_table "trips_users", force: :cascade do |t|
+    t.bigint "trip_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["trip_id"], name: "index_trips_users_on_trip_id"
     t.index ["user_id"], name: "index_trips_users_on_user_id"
   end
