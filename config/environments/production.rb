1ed287bc99a898ec4240bca9e56c4fdeab916915
Joseph Lee <jhjlee702@gmail.com>
2025-04-05 15:52:41 -0600

    set host for dev and prod environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # going to need to add the host to the domain here when I deploy
+  # config.action_mailer.default_url_options = { host: 'your-production-domain.com' }
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
