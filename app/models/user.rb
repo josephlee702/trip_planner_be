0597238c044966e8b3e6fe0dd235b257be98b3a2
Joseph Lee <jhjlee702@gmail.com>
2025-04-05 16:29:26 -0600

    copy over everything from other project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
-  include DeviseTokenAuth::Concerns::User
-
   has_many :trips_users
   has_many :trips, through: :trips_users
 
-  validates :email, presence: true, uniqueness: true
-  #on: :create makes sure the password validation only happens when creating a new user
+  extend Devise::Models
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
 end
