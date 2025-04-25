f356b174ebca0ab086a440f0f9de6ac8b082ee4d
Joseph Lee <jhjlee702@gmail.com>
2025-04-25 16:14:32 -0600

    change date validations

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -2,7 +2,6 @@ class Itinerary < ApplicationRecord
   belongs_to :trip
 
   validates :name, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
+  validates :date, presence: true
   validates :description, presence: true
 end
