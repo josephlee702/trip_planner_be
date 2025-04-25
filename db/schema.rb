fff2fd6e00f4afe4c2aefb1b6009c8a8408fa9be
Joseph Lee <jhjlee702@gmail.com>
2025-04-25 16:05:34 -0600

    update itinerary date field

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_05_232858) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_25_220403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "itineraries", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.date "start_date"
-    t.date "end_date"
+    t.date "date"
     t.bigint "trip_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
